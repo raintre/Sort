bebc86e1d22a330646dc871a815cb1f02420c974
raintre <33364187+raintre@users.noreply.github.com>
2019-08-08 09:13:14 +0900

    Add files via upload

---
+++ qsort improve.c
@@ -0,0 +1,24 @@
+#define BITSUPERWORD 32
+#define SHIFT 5
+#define MASK 0x1F
+#define N 10000000
+
+#include<stdio.h>
+
+int a[1 + N/BITSUPERWORD];
+void set (int i){ a[i>>SHIFT] |= (1<<(i & MASK)); } 
+void clr (int i){ a[i>>SHIFT] &= (1<<(i & MASK)); }
+int test (int i){ return a[i>>SHIFT] & (1<<(i & MASK)); }
+
+int main(void)
+{
+	int i;
+	for(i = 0; i < N; i++)
+		clr(i);
+	while(scanf("%d", &i) != EOF)
+		set(i);
+	for(i = 0; i < N; i++)
+		if (test(i))
+			printf("%d\n", i);
+	return 0;
+}
